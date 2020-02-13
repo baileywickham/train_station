a0ac188db7ac9681d2aca7879e5af828a55e4b85
baileywickham <baileywickham@gmail.com>
2020-02-12 21:27:52 -0800

    update to use runner lib

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/baileywickham/train_station
 
 go 1.12
 
-require github.com/lib/pq v1.3.0
+require (
+	github.com/baileywickham/runner v0.0.0-20200213044534-71ebfc6eada1
+	github.com/lib/pq v1.3.0
+)
--- runner.go
+++ runner.go
@@ -2,11 +2,7 @@ package main
 
 // cli runner
 import (
-	"bufio"
-	"log"
-	"os"
-	"strconv"
-	"strings"
+	r "github.com/baileywickham/runner"
 )
 
 func print_all_users() {
@@ -14,76 +10,39 @@ func print_all_users() {
 		account.Print()
 	}
 }
-
-func runner() {
-	reader := bufio.NewReader(os.Stdin)
-	defer func() {
-		// function to loop on panics
-		if r := recover(); r != nil {
-			log.Println("Panic: ", r)
-			runner()
-		}
-	}()
-
-	println("Entering shell")
-	for {
-		print(":|:  ")
-		text, _ := reader.ReadString('\n')
-		//text = strings.Replace(text, "\n", "", -1)
-		tokens := strings.Fields(text)
-		if len(tokens) == 0 {
-			print_help()
-			continue
-		}
-		switch tokens[0] {
-		case "c":
-			i, _ := strconv.Atoi(tokens[2])
-			account := NewAccount(tokens[1], i)
-			println("Account ID: ", account.UUID)
-		case "a":
-			// add money
-			i, _ := strconv.Atoi(tokens[2])
-			uuid, _ := strconv.Atoi(tokens[1])
-			account, err := account_by_uuid(uuid)
-			if err != nil {
-				log.Println(err.Error())
-			}
-			account.Add_balance(i)
-		case "u":
-			// use transaction. Default 1 dollar
-			uuid, _ := strconv.Atoi(tokens[1])
-			account, err := account_by_uuid(uuid)
-			if err != nil {
-				log.Println(err.Error())
-			}
-			err = account.Charge(1)
-			if err != nil {
-				log.Println(err.Error())
-			}
-		case "p":
-			uuid, _ := strconv.Atoi(tokens[1])
-			account, err := account_by_uuid(uuid)
-			if err != nil {
-				log.Println(err.Error())
-			}
-			account.Print()
-		case "pa":
-			print_all_users()
-		case "pad":
-			for _, account := range get_all_accounts_db() {
-				account.Print()
-			}
-
-		default:
-			print_help()
-		}
+func add_money_uuid(uuid, balance int) {
+	account, err := account_by_uuid(uuid)
+	if err != nil {
+		println(err.Error())
+	}
+	account.Add_balance(balance)
+}
+func charge_account_uuid(uuid int) {
+	account, err := account_by_uuid(uuid)
+	if err != nil {
+		println(err.Error())
+	}
+	err = account.Charge(-1)
+	if err != nil {
+		println(err.Error())
 	}
 }
-func print_help() {
-	println("Help:")
-	println("c : create account, name, amount")
-	println("a : add money, uuid, amount")
-	println("u : use card, uuid")
-	println("p : print account, uuid")
-	println("pa: print all accounts")
+
+func print_account_uuid(uuid int) {
+	account, err := account_by_uuid(uuid)
+	if err != nil {
+		println(err.Error())
+	}
+	account.Print()
+}
+
+func runner() {
+	shell := r.NewShell()
+	c1 := r.Command{"create_account", create_account, "Create new account [name] [amount]"}
+	c2 := r.Command{"add", add_money_uuid, "Add money to an account [uuid] [amount]"}
+	c3 := r.Command{"use", charge_account_uuid, "Charge account $1 [uuid]"}
+	c4 := r.Command{"print", print_account_uuid, "Print single account [uuid]"}
+	c5 := r.Command{"print_all", print_all_users, "Print all accounts"}
+	shell.Add_command(c1, c2, c3, c4, c5)
+	shell.Start()
 }
