3d5a2e4854d1b0c818bc154d8bfe7c06a535a2ac
baileywickham <baileywickham@gmail.com>
2020-03-01 13:04:24 -0800

    add bang

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 docker build -t train_station . && docker run --network=host -it train_station
--- pgstart.sh
+++ pgstart.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 docker build -t pg -f Dockerfile_pg . && docker run -d -p 5432:5432 pg
